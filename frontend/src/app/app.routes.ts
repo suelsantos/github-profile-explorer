babeba2f9986e00dd5e9b811cb2072c302d5c0be
Maxuel <suelsantos_mrs@hotmail.com>
2025-02-28 11:53:36 -0300

    feat: init commit

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { FollowingComponent } from './following/following.component';
+
+export const routes: Routes = [
+  { path: 'user/:username', component: UserComponent },
+  { path: 'user/:username/followings', component: FollowingComponent },
+  { path: '', redirectTo: '/user/joaorca', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
